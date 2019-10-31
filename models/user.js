669964babdb671bf13c612fb6ae735bf7b7266df
Angel Lando <angel.lando@outlook.com>
2019-10-31 11:29:43 +0100

    Added user schema, GET and POST routes

--- models/user.js
+++ models/user.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 // Define the schema for users
 const userSchema = new Schema({
-  name: String
+  username: String,
+  password: String,
+  created_at: { type: Date, default: Date.now }
 });
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
