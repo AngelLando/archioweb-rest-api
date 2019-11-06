f55faee3d0b68fdf18e2fc328fbe808162467662
Pauline Baeni <pauline.baeni@heig-vd.ch>
2019-11-06 15:51:56 +0100

    added validation

--- models/guess.js
+++ models/guess.js
@@ -4,11 +4,36 @@ const Schema = mongoose.Schema;
 const guessSchema = new Schema({
   thumbnail_id: Schema.Types.ObjectId,
   user_id: Schema.Types.ObjectId,
-  latitude: Number,
-  longitude: Number,
-  score: Number,
+  location: {
+    type: {
+      type: String,
+      required: true,
+      enum: [ 'Point' ]
+    },
+    coordinates: {
+      type: [ Number ],
+      required: true,
+      validate: {
+        validator: validateGeoJsonCoordinates,
+        message: '{VALUE} is not a valid longitude/latitude(/altitude) coordinates array'
+      }
+    }
+  },
+  score: {
+  	type: Number,
+  	min: [0, 'Score trop bas'],
+  }
   created_at: { type: Date, default: Date.now },
 
 });
+
+// Create a geospatial index on the location property.
+geolocatedSchema.index({ location: '2dsphere' });
+
+// Validate a GeoJSON coordinates array (longitude, latitude and optional altitude).
+function validateGeoJsonCoordinates(value) {
+  return Array.isArray(value) && value.length >= 2 && value.length <= 3 && value[0] >= -180 && value[0] <= 180 && value[1] >= -90 && value[1] <= 90;
+}
 // Create the model from the schema and export it
 module.exports = mongoose.model('Guess', guessSchema);
+
--- models/thumbnail.js
+++ models/thumbnail.js
@@ -4,9 +4,31 @@ const Schema = mongoose.Schema;
 const thumbnailSchema = new Schema({
   title: String,
   img: { data: Buffer, contentType: String },
-  latitude: Number,
-  longitude: Number,
+	location: {
+		type: {
+			type: String,
+			required: true,
+			enum: [ 'Point' ]
+		},
+		coordinates: {
+			type: [ Number ],
+			required: true,
+			validate: {
+				validator: validateGeoJsonCoordinates,
+				message: '{VALUE} is not a valid longitude/latitude(/altitude) coordinates array'
+			}
+		}
+	},
   created_at: { type: Date, default: Date.now }
 });
+
+// Create a geospatial index on the location property.
+geolocatedSchema.index({ location: '2dsphere' });
+
+// Validate a GeoJSON coordinates array (longitude, latitude and optional altitude).
+function validateGeoJsonCoordinates(value) {
+  return Array.isArray(value) && value.length >= 2 && value.length <= 3 && value[0] >= -180 && value[0] <= 180 && value[1] >= -90 && value[1] <= 90;
+}
 // Create the model from the schema and export it
 module.exports = mongoose.model('Thumbnail', thumbnailSchema);
+
