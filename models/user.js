cb91bf48b4fddb425ecac64646eef9c494c9d0be
Angel Lando <angel.lando@outlook.com>
2019-11-07 00:23:32 +0100

    Improved user model validation

--- models/user.js
+++ models/user.js
@@ -6,7 +6,15 @@ const userSchema = new Schema({
     type: String,
     required: true,
     minlength: [3, 'Name is too short, 3 characters minimum.'],
-    maxlength: [20, 'Name is too long, 20 characters maximum.']
+    maxlength: [20, 'Name is too long, 20 characters maximum.'],
+    unique: true,
+    validate:
+      // Manually validate uniqueness to send a "pretty" validation error
+      // rather than a MongoDB duplicate key error
+      [{
+        validator: validateUsernameUniqueness,
+        message:'Username {VALUE} already exists'
+      }],
   },
   password: {
     type: String,
@@ -14,5 +22,16 @@ const userSchema = new Schema({
   },
   created_at: { type: Date, default: Date.now }
 });
+
+/**
+ * Given a name, calls the callback function with true if no person exists with that name
+ * (or the only person that exists is the same as the person being validated).
+ */
+function validateUsernameUniqueness(value) {
+  return this.constructor.findOne().where('username').equals(value).exec().then((existingPerson) => {
+    return !existingPerson || existingPerson._id.equals(this._id);
+  });
+}
+
 // Create the model from the schema and export it
 module.exports = mongoose.model('User', userSchema);
