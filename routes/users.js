3ec83cdb5435e1492e5beb739bbdb452f40d4228
Angel Lando <angel.lando@outlook.com>
2019-11-07 09:04:07 +0100

    Retrieving a user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
+const debug = require('debug')('demo:user');
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const User = require('../models/user');
 
 /* GET users listing. */
@@ -11,6 +14,13 @@ router.get('/', function(req, res, next) {
     res.send(users);
   });
 });
+
+/* GET retrieve a user */
+router.get('/:id', loadUserFromParamsMiddleware, function(req, res, next) {
+    res.send({
+      ...req.user.toJSON()
+    });
+  });
 
 /* POST new user */
 router.post('/', function(req, res, next) {
@@ -25,5 +35,35 @@ router.post('/', function(req, res, next) {
     res.send(savedUser);
   });
 });
+
+/**
+ * Middleware that loads the user corresponding to the ID in the URL path.
+ * Responds with 404 Not Found if the ID is not valid or the user doesn't exist.
+ */
+function loadUserFromParamsMiddleware(req, res, next) {
+
+  const userId = req.params.id;
+  if (!ObjectId.isValid(userId)) {
+    return personNotFound(res, userId);
+  }
+
+  User.findById(req.params.id, function(err, user) {
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      return userNotFound(res, userId);
+    }
+
+    req.user = user;
+    next();
+  });
+}
+
+/**
+ * Responds with 404 Not Found and a message indicating that the person with the specified ID was not found.
+ */
+function userNotFound(res, userId) {
+  return res.status(404).type('text').send(`No user found with ID ${userId}`);
+}
 
 module.exports = router;
