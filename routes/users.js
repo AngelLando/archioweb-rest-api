4a2667c671a2a25c4e9d7c36e57914afa2eba550
Gioianac <joy.oppliger@heig-vd.ch>
2019-11-07 12:04:34 +0100

    Add aggregate function and paginate

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const User = require('../models/user');
 const utils = require('./utils');
 
-/* GET users listing. */
+/*GET users listing. *//*
 router.get('/', function(req, res, next) {
   User.find().sort('created_at').exec(function(err, users) {
     if (err) {
@@ -14,14 +14,7 @@ router.get('/', function(req, res, next) {
     }
     res.send(users);
   });
-});
-
-/* GET retrieve a user */
-router.get('/:id', loadUserFromParamsMiddleware, function(req, res, next) {
-    res.send({
-      ...req.user.toJSON()
-    });
-  });
+});*/
 
 /* POST new user */
 router.post('/', function(req, res, next) {
@@ -36,6 +29,80 @@ router.post('/', function(req, res, next) {
     res.send(savedUser);
   });
 });
+
+router.get('/', function(req, res, next) {
+
+  const countQuery = queryUsers(req);
+  countQuery.countDocuments(function(err, total) {
+    if (err) {
+      return next(err);
+    }
+
+    // Parse pagination parameters from URL query parameters.
+    const { page, pageSize } = utils.getPaginationParameters(req);
+
+    User.aggregate([
+      {
+        $lookup: {
+          from: 'guesses',
+          localField: '_id',
+          foreignField: 'user_id',
+          as: 'obtainedScores',
+        }
+      },
+      {
+        $unwind: '$obtainedScores'
+      },
+      {
+        $group: {
+          _id: '$_id',
+          username: { $first: '$username' },
+          createdAt: { $first: '$createdAt' },
+          totalScore: { $sum: '$obtainedScores.score' },
+          maxScore: { $max: '$obtainedScores.score' },
+          averageScore: { $avg: "$obtainedScores.score" }
+        }
+      },
+      {
+        $sort: {
+          obtainedScores: 1
+        }
+      },
+      {
+        $skip: (page - 1) * pageSize
+      },
+      {
+        $limit: pageSize
+      }
+    ], (err, users) => {
+      if (err) {
+        return next(err);
+      }
+
+      utils.addLinkHeader('/api/users', page, pageSize, total, res);
+
+      res.send(users.map(user => {
+
+        // Transform the aggregated object into a Mongoose model.
+        const serialized = new User(user).toJSON();
+
+        // Add the aggregated property.
+        serialized.totalScore = user.totalScore;
+        serialized.maxScore = user.maxScore;
+        serialized.averageScore = user.averageScore;
+
+        return serialized;
+      }));
+    });
+  });
+});
+
+/* GET retrieve a user */
+router.get('/:id', loadUserFromParamsMiddleware, function(req, res, next) {
+    res.send({
+      ...req.user.toJSON()
+    });
+  });
 
 /* PATCH a user */
 router.patch('/:id', utils.requireJson, loadUserFromParamsMiddleware, function(req, res, next) {
@@ -94,10 +161,21 @@ function loadUserFromParamsMiddleware(req, res, next) {
 }
 
 /**
- * Responds with 404 Not Found and a message indicating that the person with the specified ID was not found.
+ * Responds with 404 Not Found and a message indicating that the user with the specified ID was not found.
  */
 function userNotFound(res, userId) {
   return res.status(404).type('text').send(`No user found with ID ${userId}`);
+}
+
+function queryUsers(req) {
+
+  let query = User.find();
+/*
+  if (typeof(req.query.username) == 'string') {
+    query = query.where('username').equals(req.query.username);
+  }*/
+
+  return query;
 }
 
 module.exports = router;
