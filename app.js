3dcb687cac36d9b7a721898f341d3909690fde63
Angel Lando <angel.lando@outlook.com>
2019-11-12 14:32:18 +0100

    Fixed logs display

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(logger('dev'));
 }
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
